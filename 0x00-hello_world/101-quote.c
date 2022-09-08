0b554da649eacf08dedc69820d2f18a6621126a8
Harp <walesii19@gmail.com>
2022-09-08 19:04:06 +0000

    Fixed error in 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
+#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
 /**
  * main - Entry point
  *
- * Return Always 0 (Success)
+ * Return: Always 1
  */
-int main (void)
+int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19",
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
 		strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 
 	return (1);
