cba57a34a6b65826ee624898b2229200b48425c3
Harp <walesii19@gmail.com>
2022-09-08 14:57:50 +0000

    Updated tasks 1 to 6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+/* Prints "Programming is like building a multilingual puzzle exactly */
+#include <stdio.h>
+
+int main()
+{
+	char mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(mystr);
+
+	return 0;
+}
