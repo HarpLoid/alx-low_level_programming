ff4e54315e52522b6facf9a6646de4252f2b4caf
Harp <walesii19@gmail.com>
2022-09-12 10:22:13 +0000

    initial commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations
+ * of two two-digit numbers.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int tens, units, t, u;
+
+	for (tens = '0'; tens <= '9'; tens++)
+	{
+		for (units = '0'; units <= '9'; units++)
+		{
+			for (t = tens; t <= '9'; t++)
+			{
+				for (u = units + 1; u <= '9'; u++)
+				{
+					putchar(tens);
+					putchar(units);
+					putchar(' ');
+					putchar(t);
+					putchar(u);
+
+					if (!((tens == '9' && units == '8') && (t == '9' && u == '9')))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				u = '0';
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
