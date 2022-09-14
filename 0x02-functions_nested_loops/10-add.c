ed8a23a16caf68120c8372f1f5a94e00ef596a54
Harp <walesii19@gmail.com>
2022-09-14 01:17:10 +0000

    initial commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integer and displays the result
+ * @a: integer arguement
+ * @b: integer arguement
+ *
+ * Return: value of addition.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
