6bdd6138c80884c9e6fcab7f7c2f8f15e603e227
Harp <walesii19@gmail.com>
2022-09-16 00:18:45 +0000

    fixed betty style

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers
+ * print_most_numbers - prints numbers
  * from 0 to 9 excluding 2 and 4
  *
  * Return: Always 0 (success)
  */
 void print_most_numbers(void)
 {
-       char n = '0';
+	char n = '0';
 
 	while (n <= '9')
 	{
