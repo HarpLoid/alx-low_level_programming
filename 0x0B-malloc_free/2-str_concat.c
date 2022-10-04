b1a6f13b5dc227e16640446656e48f97a2b94d83
Harp <walesii19@gmail.com>
2022-10-04 11:35:16 +0000

    Fixed Output error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,6 +15,15 @@ char *str_concat(char *s1, char *s2)
 	int len_s1 = 0, len_s2 = 0;
 	char *cat_str;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	for (i = 0; s1[i]; i++)
 	{
 		len_s1++;
