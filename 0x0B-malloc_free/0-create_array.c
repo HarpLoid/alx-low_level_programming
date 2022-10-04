4f0299facb9b6174699b31a739e7fb8c49cffa4c
Harp <walesii19@gmail.com>
2022-10-04 00:30:43 +0000

    fixed style and set check for malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,8 +18,12 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	
+
 	arr = malloc(size * sizeof(char));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 	{
