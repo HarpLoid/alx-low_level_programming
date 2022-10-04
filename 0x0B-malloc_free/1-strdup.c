56433f6331b51f07147bf5f87107aacabda59f7c
Harp <walesii19@gmail.com>
2022-10-04 01:07:40 +0000

    fixed incorrect return

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen - finds the length of a string
- * @s: pointer to character string
- *
- * Return: value of length of string.
- */
-int _strlen(char *s)
-{
-	int i, len = 0;
-
-	for (i = 0; s[i] < '\0'; i++)
-	{
-		len++;
-	}
-
-	return (len);
-}
-
-/**
  * _strdup - duplicates a string
  * to an allocated memory location.
  * @str: pointer to a string.
@@ -29,25 +11,31 @@ int _strlen(char *s)
 char *_strdup(char *str)
 {
 	int i;
-	int l = _strlen(str);
+	int l;
 	char *s;
 
-	if (str == NULL)
+	if (str == NULL) /*Validates str in the function*/
 	{
 		return (NULL);
 	}
+	
+	for (i = 0; str[i] < '\0'; i++) /*Checks the number of elements in str*/
+	{
+		l++;
+	}
+	l++;
 
-	s = malloc(sizeof(str) * sizeof(char));
+	s = malloc(l * sizeof(char));
+	
 	if (s == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; i <= l; i++)
+	for (i = 0; i < l; i++)
 	{
 		s[i] = str[i];
 	}
 
 	return (s);
-	free(s);
 }
