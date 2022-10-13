a60c69826d8372c2e1ebb582fa7338794f2e6a0c
Harp <walesii19@gmail.com>
2022-10-13 21:11:02 +0000

    fixed the if statements

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,24 +13,21 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+	int num;
 	va_list ap;
 
 	va_start(ap, n);
 
-	if (separator == NULL)
-	{
-		return;
-	}
-
 	for (i = 0; i < n; i++)
 	{
-		if (i != (n - 1))
+		num = va_arg(ap, int);
+		if (i != (n - 1) && separator != NULL)
 		{
-			printf("%d%s", va_arg(ap, int), separator);
+			printf("%d%s", num, separator);
 		}
 		else
 		{
-			printf("%d", va_arg(ap, int));
+			printf("%d", num);
 		}
 	}
 	printf("\n");
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,27 +18,23 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_start(ap, n);
 
-	if (separator == NULL)
-	{
-		return;
-	}
-
+	
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(ap, char*);
 
 		if (str == NULL)
 		{
-			printf("nil");
-		}
-
-		if (i != (n - 1))
-		{
-			printf("%s%s", str, separator);
+			printf("(nil)");
 		}
 		else
 		{
 			printf("%s", str);
+		}
+
+		if (i != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
 		}
 	}
 	printf("\n");
