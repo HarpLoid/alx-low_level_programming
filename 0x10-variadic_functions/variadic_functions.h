901afd0d254e597628cfdaf83eb6ac416a7b9e44
Harp <walesii19@gmail.com>
2022-10-14 03:51:33 +0000

    initial commit and updated header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -37,6 +37,12 @@ void print_numbers(const char *separator, const unsigned int n, ...);
  */
 void print_strings(const char *separator, const unsigned int n, ...);
 
-
+/**
+ * print_all - prints anything
+ * @format: format of argument pased
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...);
 
 #endif /*_VARIADIC_FUNCTIONS_H_*/
