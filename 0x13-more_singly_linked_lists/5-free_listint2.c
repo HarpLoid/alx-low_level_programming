3b235d2de50cc72b62cd420f1cd8856cc06ed0d3
Harp <walesii19@gmail.com>
2022-10-24 19:54:28 +0000

    fixed error

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (head)
+	while (*head)
 	{
 		temp = *head;
-		*head = *head->next;
+		*head = temp->next;
 		free(temp);
 	}
 	*head = NULL;
