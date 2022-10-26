15a24f3592aee4e05a316dec3692192d8389d353
Harp <walesii19@gmail.com>
2022-10-26 14:32:46 +0000

    initial commit, updated header file

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete the node at the
+ * given index.
+ * @head: head pointer of list
+ * @index: given index to delete.
+ *
+ * Return: 1 on success and -1 on failure
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current, *prev, *temp;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+
+	current = *head;
+
+	if (index == 0)
+	{
+		*head = current->next;
+		current->next = NULL;
+		free (current);
+		return (1);
+	}
+
+	while (current)
+	{
+		if (i == index)
+		{
+			temp = current;
+			prev->next = current->next;
+			temp->next = NULL;
+			free (temp);
+			return (1);
+		}
+		prev = current;
+		current = current->next;
+		i++;
+	}
+	return (-1);
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts node at
+ * a given index
+ * @head: head pointer
+ * @idx: index to insert node at
+ * @n: data to add
+ *
+ * Return: address of newnode.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *current, *prev, *newnode;
+	unsigned int i = 0;
+
+	newnode = malloc(sizeof(listint_t));
+	if (!(newnode))
+	{
+		return (NULL);
+	}
+	newnode->n = n;
+	newnode->next = NULL;
+
+	current = *head;
+	while (current)
+	{
+		prev = current;
+		current = current->next;
+		if (i == (idx - 1))
+		{
+			prev->next = newnode;
+			newnode->next = current;
+		}
+		i++;
+	}
+	if (i < idx)
+	{
+		return (NULL);
+	}
+	return (newnode);
+}
