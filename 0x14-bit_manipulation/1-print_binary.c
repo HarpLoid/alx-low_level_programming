c71e4d6498e96a1000366c55b9bfddd97661e9bc
Harp <walesii19@gmail.com>
2022-10-28 23:36:59 +0000

    Initial commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,7 +12,7 @@ void print_binary(unsigned long int n)
 	unsigned long int n_dup = n, mask = 1;
 	int len = 0;
 
-	if (!n)
+	if (n == 0)
 	{
 		_putchar('0');
 		return;
@@ -37,6 +37,6 @@ void print_binary(unsigned long int n)
 		else
 			_putchar('0');
 
-		mask >>= 1
+		mask >>= 1;
 	}
 }
