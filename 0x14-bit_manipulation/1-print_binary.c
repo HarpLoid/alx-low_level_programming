160de0ee33e318c8628c4848fccafa4861d7418d
Harp <walesii19@gmail.com>
2022-10-28 23:30:24 +0000

    Initial commit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary
+ * representation of a number
+ * @n: integer to be represented
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int n_dup = n, mask = 1;
+	int len = 0;
+
+	if (!n)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (n_dup)
+	{
+		n_dup >>= 1;
+		len++;
+	}
+	len -= 1;
+
+	if (len)
+	{
+		mask <<= len;
+	}
+
+	while (mask)
+	{
+		if (n & mask)
+			_putchar('1');
+		else
+			_putchar('0');
+
+		mask >>= 1
+	}
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b);
+int _strlen(const char *s);
+int _pow(int base, unsigned int power);
+int _putchar(char c);
+void print_binary(unsigned long int n);
+
+
+
+#endif/*MAIN_H*/
