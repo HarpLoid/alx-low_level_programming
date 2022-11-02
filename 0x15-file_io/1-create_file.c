b3a248d2db338db0bdaa2e06eec191305e334370
Harp <walesii19@gmail.com>
2022-11-02 12:52:59 +0000

    fixed betty style error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,7 @@
  * create_file - creates a file
  * @filename: name of file to create
  * @text_content: text to put in file
- * 
+ *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
