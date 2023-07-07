1d70ac486a5c5327e49ece7e3fe04b38262fca61
HarpLoid <walesii19@gmail.com>
2023-07-07 18:52:45 +0100

    fixed betty style error

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,41 +1,20 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds an element to the
+ * create_node - adds an element to the
  * hash table
  * @ht: the hash table to add or update the key/value to
  * @key: the key
  * @value: value associated with the key
+ * @index: index of the key in the array
  *
  * Return: 1 if it succeeded, 0 otherwise
  */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+int create_node(hash_table_t *ht, const char *key,
+				const char *value, unsigned long int index)
 {
-	hash_node_t *idx_node = NULL;
-	unsigned long int index;
-	char *v;
-	char *k;
-
-	if (!ht || !(ht->array) || !key || strlen(key) == 0 || !value)
-		return (0);
-
-	index = key_index((const unsigned char *)key, ht->size);
-
-	idx_node = (ht->array)[index];
-
-	while (idx_node && (strcmp(key, idx_node->key) != 0))
-		idx_node = idx_node->next;
-
-	if (idx_node != NULL)
-	{
-		v = strdup(value);
-		if (!v)
-			return (0);
-		if (idx_node->value)
-			free(idx_node->value);
-		idx_node->value = v;
-		return (1);
-	}
+	hash_node_t *idx_node;
+	char *k, *v;
 
 	idx_node = malloc(sizeof(hash_node_t));
 	if (!idx_node)
@@ -66,4 +45,43 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	(ht->array)[index] = idx_node;
 
 	return (1);
+}
+
+/**
+ * hash_table_set - adds an element to the
+ * hash table
+ * @ht: the hash table to add or update the key/value to
+ * @key: the key
+ * @value: value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *idx_node = NULL;
+	unsigned long int index;
+	char *v;
+
+	if (!ht || !(ht->array) || !key || strlen(key) == 0 || !value)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	idx_node = (ht->array)[index];
+
+	while (idx_node && (strcmp(key, idx_node->key) != 0))
+		idx_node = idx_node->next;
+
+	if (idx_node != NULL)
+	{
+		v = strdup(value);
+		if (!v)
+			return (0);
+		if (idx_node->value)
+			free(idx_node->value);
+		idx_node->value = v;
+		return (1);
+	}
+
+	return (create_node(ht, key, value, index));
 }
--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,7 +11,7 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int index;
 	hash_node_t *node;
 	char *comma = "";
-	
+
 	putchar('{');
 	for (index = 0; index < ht->size; index++)
 	{
