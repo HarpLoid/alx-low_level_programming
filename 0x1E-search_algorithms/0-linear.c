b0a9383806f5e4bf635068942057f5df7bdb612a
HarpLoid <walesii19@gmail.com>
2023-10-11 14:28:39 +0100

    fixed betty doc issue

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * Searches for a value in
+ * linear_search - Searches for a value in
  * an array of integers using the
  * Linear Search Algorithm
  * @value: value to search for in array
